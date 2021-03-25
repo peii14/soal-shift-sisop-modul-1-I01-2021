85c330a51021794639b42d0aa96c26d67c393eb8
Gayuh Kautaman <gayuh14@yahoo.com>
2021-03-25 23:24:13 +0700

    done 3a working on 3b. continue to move file

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,3 +1,14 @@
 #!bin/bash
 
+#nama file
+	date=$(tail -1 Foto.log | awk '{print $1}' Foto.log)
+        yr=${date:1:5}
+        mo=${date:6:3}
+        dy=${date:10:3}
+        #printf "%s%s%s\n" $dy $mo $yr
 
+
+for ((iter=1;iter<=23;iter=iter+1))
+do	
+	printf "%s%s%s\n" $dy $mo $yr	
+done 
