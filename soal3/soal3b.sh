8e1a92685774233f80840790845b78bb4b055bda
Gayuh Kautaman <gayuh14@yahoo.com>
2021-03-26 18:15:01 +0700

    done 3c

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -4,10 +4,10 @@
 	date=$(tail -1 Foto.log | awk '{print $1}' Foto.log )
         yr=${date:1:5}
         mo=${date:6:3}
-        dy=${date:10:3}
+        dy=${date:10:2}
         #printf "%s%s%s\n" $dy $mo $yr
-	director="${dy}${mo}${yr}"
-	#printf "%s\n" $director
+	director="$dy$mo$yr"
+	printf "%s\n" $director
 
 
 	if [[ ! -d "$director" ]]
@@ -17,9 +17,6 @@
 
 for ((iter=1;iter<=23;iter=iter+1))
 do	
-	#finalfile=$(ls $director/ | sort -V | tail -n 1)
-	#filename=${finalfile%.*}	
-	#lastfile=${filename:9}
 	if [[ -d "$director" ]]
 	then
 		mv "Kumpulan_$iter.jpg" "$director/"
---
+++ soal3/soal3c.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+today=$(date +%m-%d-%Y)
+day=$(date +"%--j")
+
+echo $today
+
+if [ ! -d "kucing_$day" ]&&[ $(($day%2)) -eq 0 ]
+then 
+	mkdir -p "kucing_$today"
+	wget --output-document "$day.jpg" "https://loremflickr.com/320/240/kitten"
+        mv "$day.jpg" "kucing_$today/"
+
+elif [ ! -d "kelinci_$day" ]&&[ $(($day%2)) -ne 0 ]
+then
+	mkdir -p "kelinci_$today"
+	wget --output-document "$day.jpg" "https://loremflickr.com/320/240/bunny"
+        mv "$day.jpg" "kelinci_$today/"
+fi
