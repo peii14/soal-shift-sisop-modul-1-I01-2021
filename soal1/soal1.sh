212e30856a9a4ef74b5a2b8984504e347ab59d55
damdum <satriaadam04@gmail.com>
2021-03-27 00:44:46 +0700

    1b,1d - done

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,1 +1,16 @@
-echo "pler"
+#!/bin/bash
+perror=`grep -E -o 'ERROR.*' syslog.log | cut -f2- -d\ | cut -f1 -d"(" | sort | uniq -c | sort -n -r`
+        #ngambil dari ERROR ke belakang | ngambil huruf| ngilangin uname| ngrutin yg udah di cut | ngitung kata ulang yg udah diurutin| ngurutin berdasarkan angka dari gede
+        #                               | setelah error|   
+
+echo "$perror"     
+header="Error_Count";
+
+echo "$perror" | while read error_check
+do
+    #output jumlah error per tipe error
+    count=`echo $error_check | cut -d ' ' -f 1`
+    #output tipe error
+ 	error_type=`echo $error_check | cut -d ' ' -f 2-`
+	echo "$error_type,$count" 
+done | sed '1 i\'$header > error_message.csv
