a92ffd33537c232888c161209394dbc3d8d83892
damdum <satriaadam04@gmail.com>
2021-03-27 14:53:36 +0700

    final 1b 1d

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-perror=`grep -E -o 'ERROR.*' syslog.log | cut -f2- -d\ | cut -f1 -d"(" | sort | uniq -c | sort -n -r`
+error_counter=`grep -E -o 'ERROR.*' /home/damdum/syslog.log | cut -f2- -d\ | cut -f1 -d"(" | sort | uniq -c | sort -n -r`
         #ngambil dari ERROR ke belakang | ngambil huruf| ngilangin uname| ngrutin yg udah di cut | ngitung kata ulang yg udah diurutin| ngurutin berdasarkan angka dari gede
-        #                               | setelah error|   
+        #                               | setelah error ke belakang|   
 
-echo "$perror"     
-header="Error_Count";
+#echo "$error_counter"
 
-echo "$perror" | while read error_check
-do
-    #output jumlah error per tipe error
-    count=`echo $error_check | cut -d ' ' -f 1`
-    #output tipe error
- 	error_type=`echo $error_check | cut -d ' ' -f 2-`
-	echo "$error_type,$count" 
-done | sed '1 i\'$header > error_message.csv
+echo "$error_counter" | while read error_check
+        do
+                #output jumlah error per tipe error
+                count=`echo $error_check | cut -f1 -d ' '`
+                #echo "$count"
+                #output tipe error
+ 	        error_type=`echo $error_check | cut -d ' ' -f2-`
+	        echo "$error_type,$count" 
+              #masukin header error_count di line pertama make sed
+        done | sed '1i Error_Count' > error_message.csv
