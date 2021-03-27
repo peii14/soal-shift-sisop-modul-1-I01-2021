aebe182c586e66794695f88ade42321f6ddf3e31
Gayuh Kautaman <gayuh14@yahoo.com>
2021-03-27 14:27:09 +0700

    soal 3 done

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -1,17 +1,20 @@
 #!bin/bash
+#set todays date and take the day from the year count 
 today=$(date '+%d-%m-%y')
 day=$(date +"%--j")
 
 echo $today
-
+#if ther is no kucing folder for this day and the day is even
 if [ ! -d "kucing_$day" ]&&[ $(($day%2)) -eq 0 ]
-then 
+then 	
+	#create the folder download kitten and move it to the directory that are made
 	mkdir -p "kucing_$today"
 	wget --output-document "$day.jpg" "https://loremflickr.com/320/240/kitten"
         mv "$day.jpg" "kucing_$today/"
-
+#when kelinci folder for this day is not reated and day is odd
 elif [ ! -d "kelinci_$day" ]&&[ $(($day%2)) -ne 0 ]
 then
+	#create the folder download bunny and move it to kelinci this day folder
 	mkdir -p "kelinci_$today"
 	wget --output-document "$day.jpg" "https://loremflickr.com/320/240/bunny"
         mv "$day.jpg" "kelinci_$today/"
